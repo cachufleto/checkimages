2af2543ed3c75fed14ff32b78f9943e7eae1fd40
cachufleto <carlos.paz@free.fr>
2016-06-09 14:38:21 +0200

    clean

--- Conf/Bdd.inc.old.php
+++ Conf/Bdd.inc.old.php
@@ -11,5 +11,5 @@ define ('USERBDD', 'root'); // user
 define ('MDPBDD', ''); // m. de p.
 define ('SURFIMAGE', 'ceido_surfimages'); // base des images externes
 define ('MEDICAMENTS', 'ceido_medicaments'); // base des medicaments
-define ('PARAPHARMACIE', 'ceido_parpharmacie'); // base des parapharmacies
+define ('PARAPHARMACIE', 'ceido_parapharmacie'); // base des parapharmacies
 
