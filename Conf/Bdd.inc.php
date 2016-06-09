cf210c48e67fc227c59494b77a44b4d4a6705201
cachufleto <carlos.paz@free.fr>
2016-06-09 13:36:10 +0200

    Ignorer BDD

---
+++ Conf/Bdd.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 09/06/2016
+ * Time: 13:28
+ */
+
+define ('HOSTBDD', '127.0.0.1'); // Host
+define ('USERBDD', 'root'); // user
+define ('MDPBDD', ''); // m. de p.
+define ('SURFIMAGE', 'ceido_surfimages'); // base des images externes
+define ('MEDICAMENTS', 'ceido_medicaments'); // base des medicaments
+define ('PARAPHARMACIE', 'ceido_parpharmacie'); // base des parapharmacies
+
--- Conf/param.php
+++ Conf/param.php
@@ -5,14 +5,6 @@
  * Date: 31/05/2016
  * Time: 14:51
  */
-
-define ('HOSTBDD', '127.0.0.1'); // Host
-define ('USERBDD', 'root'); // user
-define ('MDPBDD', ''); // m. de p.
-define ('SURFIMAGE', 'ceido_surfimages'); // base des images externes
-define ('MEDICAMENTS', 'ceido_medicaments'); // base des medicaments
-define ('PARAPHARMACIE', 'ceido_parpharmacie'); // base des parapharmacies
-
 define('RACINE' , dirname(__DIR__).'/');
 define('APP', RACINE . 'App/');
 define('FUNC', RACINE . 'Functions/');
@@ -20,4 +12,6 @@ define('CONF', RACINE . 'Conf/');
 define('VUE', APP . 'Vues/');
 define('PHOTO', RACINE . 'photos/');
 define('NUM', 16);
+
+include_once ('Bdd.inc.php');
 
