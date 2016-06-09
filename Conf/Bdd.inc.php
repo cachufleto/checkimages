72aeefba7a2c960d1cdf205210d90230480a6c3a
cachufleto <carlos.paz@free.fr>
2016-06-09 14:23:39 +0200

    clean

--- Conf/Bdd.inc.php
+++ Conf/Bdd.inc.php
@@ -9,7 +9,7 @@
 define ('HOSTBDD', '127.0.0.1'); // Host
 define ('USERBDD', 'root'); // user
 define ('MDPBDD', ''); // m. de p.
-define ('SURFIMAGE', 'ceido_surfimages'); // base des images externes
-define ('MEDICAMENTS', 'ceido_medicaments'); // base des medicaments
-define ('PARAPHARMACIE', 'ceido_parpharmacie'); // base des parapharmacies
+define ('SURFIMAGE', 'ceidotest_surfimages'); // base des images externes
+define ('MEDICAMENTS', 'ceidotest_medicaments'); // base des medicaments
+define ('PARAPHARMACIE', 'ceidotest_parpharmacie'); // base des parapharmacies
 
